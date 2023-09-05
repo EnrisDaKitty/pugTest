8dddf59e7acf06f16e449a736e14f1199d0d2575
Naven Rosales <nrosales@student.neumont.edu>
2023-09-05 14:16:19 -0600

    feff

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,12 @@
-// Add Express
-const express = require("express");
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
 
 // Initialize Express
 const app = express();
